c30557433299de36b07d6735a032434e7741a1b9
Marcel Braghetto <lilarcordev@gmail.com>
2015-10-04 20:04:29 +1300

    Updated readme to have some info about the blog that the demos relate to.

---
+++ FlatBuffers/redhotcompiler/src/main/java/io/github/marcelbraghetto/redhotcompiler/RedHotSetter.java
@@ -0,0 +1,11 @@
+package io.github.marcelbraghetto.redhotcompiler;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Marcel Braghetto on 21/09/15.
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface RedHotSetter {
+}
